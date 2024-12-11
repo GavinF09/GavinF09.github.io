823c439875839166d23a186559956f9482467e46
GallvinF <84831466+GallvinF@users.noreply.github.com>
2024-12-11 23:31:57 +0800

    updated info

--- js/profile/experience.js
+++ js/profile/experience.js
@@ -6,9 +6,9 @@ $.getJSON(PROFILEPATH + "experience.json", (data) => {
             <div data-date="${position.date.start} - ${position.date.end}">
                 <h3>${position.title}</h3>
                 <h4>${position.company}</h4>
-                <p>
+                <!-- <p>
                     ${position.body}
-                </p>
+                </p> -->
             </div>
         `
         $("#experience-timeline").append(positionEntry)
